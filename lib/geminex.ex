c77a68304bb0de00137b851777bd88401f83d62b
Karl Seguin <k@openmymind.io>
2021-07-03 12:35:09 +0800

    SNI support

--- lib/geminex.ex
+++ lib/geminex.ex
@@ -15,7 +15,6 @@ defmodule Geminex do
 	def init(opts) do
 		# make sure this exists earlier
 		config = Keyword.merge(@config, opts)
-
 		port = Keyword.get(config, :port, 1965)
 		ip = config |> Keyword.get(:ip, "0.0.0.0") |> String.to_charlist()
 
@@ -45,14 +44,40 @@ defmodule Geminex do
 	end
 
 	defp ssl_options(nil), do: []
+
 	defp ssl_options(config) do
-		[
-			keyfile: config[:key],
-			certfile: config[:cert],
+		case Keyword.pop(config, :hosts) do
+			{nil, _} -> config |> build_ssl_config() |> common_ssl(config)
+			{hosts, config} ->
+				hosts = Enum.map(hosts, fn {host, config} ->
+					{String.to_charlist(host), build_ssl_config(config)}
+				end)
+				common_ssl([sni_hosts: hosts], config)
+		end
+	end
+
+	defp build_ssl_config(config) do
+		config = [
+			keyfile: Keyword.fetch!(config, :key),
+			certfile: Keyword.fetch!(config, :cert)
+		]
+		if !File.exists?(config[:keyfile]) do
+			raise "Missing key file: #{config[:keyfile]}"
+		end
+
+		if !File.exists?(config[:certfile]) do
+			raise "Missing cert file: #{config[:certfile]}"
+		end
+
+		config
+	end
+
+	defp common_ssl(ssl, config) do
+		Keyword.merge(ssl, [
 			cacertfile: config[:ca],
 			versions: config[:protocol] || [:"tlsv1.3"],
 			ciphers: config[:ciphers] || ["ECDHE-ECDSA-AES128-SHA256", "ECDHE-ECDSA-AES128-SHA"]
-		]
+		])
 	end
 
 end
